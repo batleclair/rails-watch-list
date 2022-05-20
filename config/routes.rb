e6ca04ccd048effdf0ec951a0b455c0ae1878b9c
Baptiste Clair <baptisteclair@gmail.com>
2022-05-20 11:34:05 +0200

    homepage route correction 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+
   resources :lists, only: %i[new index show create] do
     resources :bookmarks, only: %i[new create]
   end
