f069181bdc940345d9f26d33ba55f3663a417b27
Baptiste Clair <baptisteclair@gmail.com>
2022-05-20 16:58:33 +0200

    aall good

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[new index show create] do
     resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: [:destroy]
 end
